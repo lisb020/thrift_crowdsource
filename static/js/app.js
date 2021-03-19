99181d68d7015c3ac96f8278ddc2bcd7adc1c5ea
Lisa Stroh <lisb020@yahoo.com>
2021-03-19 12:05:46 -0700

    working on appending table

--- static/js/app.js
+++ static/js/app.js
@@ -1,20 +1,52 @@
-function buildTable() {
+// function buildTable() {
+//   /* data route */
+//   const url = "/api/clothes";
+//   d3.json(url).then(function(response) {
+
+//     console.log(response);
+
+//     const data = response[0];
+//     //const col = d3.select("#items").append("div").classed("col-md entry", true);
+//     d3.select("#items").selectAll(".entry")
+//       .data(data)
+//       .enter()
+//       .append("div")
+//       .classed("col-md entry", true)
+//       .append("p")
+//       .text(function(keys, items){data.keys});
+//       //.html(`<div class="col-md entry"><p>insert data</p></div>`)
+//   });
+// };
+// buildTable();
+function Test() {
   /* data route */
-  const url = "/api/clothes";
-  d3.json(url).then(function(response) {
+  const response = [{
+    "item": ["pants", "shirt"],
+    "size": ["3T", "6"],
+    "name": ["Lisa", "Mary"],
+    "color": ["blue", "dont care"],
+    "maxprice": [500, 20],
+    "condition": ["like new", "new"],
+    "notes": ["nothing", "i like"]
+  }];
+  console.log(response);
 
-    console.log(response);
-
-    const data = response;
+    const data = response[0];
     //const col = d3.select("#items").append("div").classed("col-md entry", true);
+    for (let i in data) {
+      console.log("i",i,"data[i]", data[i]);
+      for (let j in data[i]){
+        console.log(data[i][j])
+      }
+  }
     d3.select("#items").selectAll(".entry")
-      .data(data)
+      //.data(Object.keys(data))
+      .data(Object.entries(data))
       .enter()
       .append("div")
       .classed("col-md entry", true)
       .append("p")
-      .text(data[0]);
+      .text((d) => d[1][0]);
       //.html(`<div class="col-md entry"><p>insert data</p></div>`)
-  });
 };
-buildTable();
+Test();
