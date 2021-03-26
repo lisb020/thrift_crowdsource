c4d2f3c21f55b306afc898bdf182bb16d7d8ad0f
Lisa Stroh <lisb020@yahoo.com>
2021-03-26 13:58:51 -0700

    changing the way database is shown on index.html

--- app.py
+++ app.py
@@ -27,38 +27,9 @@ db = SQLAlchemy(app)
 
 Thrift = create_classes(db)
 
-#create route that renders index.html template
+#create route that renders index.html template and query the database and send thrift_data variable to index.html
 @app.route("/")
 def home():
-    return render_template("index.html")
-
-
-# Query the database and send the jsonified results
-@app.route("/send", methods=["GET", "POST"])
-def send():
-    if request.method == "POST":
-        item = request.form["thriftItem"]
-        size = request.form["thriftSize"]
-        name = request.form["thriftName"]
-        color = request.form["thriftColor"]
-        maxprice = float(request.form["thriftMaxprice"])
-        condition = request.form["thriftCondition"]
-        notes = request.form["thriftNotes"]
-
-        thrift = Thrift(item=item, size=size, name=name, color=color, maxprice=maxprice, condition=condition, notes=notes)
-        db.session.add(thrift)
-        db.session.commit()
-        return redirect("/", code=302)
-
-    return redirect("/", code=302)
-
-@app.route("/form")
-def form():
-    return render_template("form.html")
-
-
-@app.route("/api/clothes")
-def clothes():
     results = db.session.query(Thrift.id, Thrift.item, Thrift.size, Thrift.name, Thrift.color, Thrift.maxprice, Thrift.condition, Thrift.notes).all()
 
     id = [result[0] for result in results]
@@ -83,9 +54,63 @@ def clothes():
         "notes": notes[i],
         "id": id[i]
         })
+    return render_template("index.html", database=thrift_data)
 
-    return jsonify(thrift_data)
 
+# Send the form data to database
+@app.route("/send", methods=["GET", "POST"])
+def send():
+    if request.method == "POST":
+        item = request.form["thriftItem"]
+        size = request.form["thriftSize"]
+        name = request.form["thriftName"]
+        color = request.form["thriftColor"]
+        maxprice = float(request.form["thriftMaxprice"])
+        condition = request.form["thriftCondition"]
+        notes = request.form["thriftNotes"]
+
+        thrift = Thrift(item=item, size=size, name=name, color=color, maxprice=maxprice, condition=condition, notes=notes)
+        db.session.add(thrift)
+        db.session.commit()
+        return redirect("/", code=302)
+
+    return redirect("/", code=302)
+
+@app.route("/form")
+def form():
+    return render_template("form.html")
+
+
+# @app.route("/api/clothes")
+# def clothes():
+#     results = db.session.query(Thrift.id, Thrift.item, Thrift.size, Thrift.name, Thrift.color, Thrift.maxprice, Thrift.condition, Thrift.notes).all()
+
+#     id = [result[0] for result in results]
+#     item = [result[1] for result in results]
+#     size = [result[2] for result in results]
+#     name = [result[3] for result in results]
+#     color = [result[4] for result in results]
+#     maxprice = [result[5] for result in results]
+#     condition = [result[6] for result in results]
+#     notes = [result[7] for result in results]
+    
+#     thrift_data = []
+    
+#     for i in range(len(id)):
+#         thrift_data.append({
+#         "item": item[i],
+#         "size": size[i],
+#         "name": name[i],
+#         "color": color[i],
+#         "maxprice": maxprice[i],
+#         "condition": condition[i],
+#         "notes": notes[i],
+#         "id": id[i]
+#         })
+
+#     return jsonify(thrift_data)
+
+# Remove item from database based on id
 @app.route("/remove", methods=["GET", "POST"])
 def remove():
     if request.method == "POST":
--- static/js/app.js
+++ static/js/app.js
@@ -15,4 +15,43 @@ $('body').on('click', '.btn', function(event){
     });
   };
 });
+// function buildTable() {
+//   /* data route */
+//   const url = "/api/clothes";
+//   d3.json(url).then(function(response) {
+    
+//     keyOrder = ["item", "size", "name", "color", "maxprice","condition", "notes", "id"];
+
+//     for (let i = 0; i < response.length; i++){
+//       for (const key of keyOrder){
+//           if (key == "item"){
+//             let rowCol = d3.select(".underHeader")
+//             .append("div")
+//             .attr("class", function(){return ("row rowData" + i)})
+//             .append("div")
+//             .classed("col-md items", true)
+//             .append("p")
+//             .text(response[i][key]);
+//           }
+//           else if (key == "id"){
+//             let class2 = ".rowData" + i;
+//             let rowCol = d3.select(class2)
+//             .append("div")
+//             .classed("col-md items", true)
+//             .html(`<button type='button' class='btn btn-primary btn-sm' id= 'id${response[i][key]}'>Remove</button>`);
+//             }
+          
+//           else {
+//           let class2 = ".rowData" + i;
+//           let rowCol = d3.select(class2)
+//           .append("div")
+//           .classed("col-md items", true)
+//           .append("p")
+//           .text(response[i][key]);
+//           };
+//       };
+//     };
+//   });
+// };
+// buildTable();
 
